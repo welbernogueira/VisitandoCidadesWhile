ef1122bf7243ad607547e3c04ef84897c4dc29ba
Welber Nogueira <117238207+welbernogueira@users.noreply.github.com>
2023-07-21 21:38:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const turista = prompt("E aí, Turista! Qual é o seu nome?");
+let cidades = " ";
+let contagem = 0;
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)");
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual foi o nome da cidade visitada?");
+  cidades += " - " + cidade + "\n";
+  contagem++;
+  continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)");
+}
+
+alert(
+  "Turista: " +
+    turista +
+    "\nQuantidade de cidades visitadas: " +
+    contagem +
+    "\nCidades visitadas:\n" +
+    cidades
+);
